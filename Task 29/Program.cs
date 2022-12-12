864ecc5faebe22ccb948ef33f533f9563f4a2dae
Oleg Selivanov <alien3569@mail.ru>
2022-12-12 17:27:33 +0300

    Add fin

---
+++ Task 29/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 29: 
+Напишите программу, которая спрашивает у пользователя кол-во элементов массива,
+затем сами элементы. Программ должна вывести массив на экран.
+
+8
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите кол-во элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа массива: ");
+
+int[] array = new int[N];
+
+    for (int i = 0; i < N; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+Console.WriteLine($"{String.Join(", ", array)}");
+Console.WriteLine($"Введённый массив: [{String.Join(", ", array)}]");
+     
